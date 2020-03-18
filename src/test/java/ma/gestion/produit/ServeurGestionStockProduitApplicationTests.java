a0301c402482c1697ae59a44862700bf5954d2b8
saad <saad.belefqih@gmail.com>
2020-03-18 13:16:41 +0000

    first commit

---
+++ src/test/java/ma/gestion/produit/ServeurGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.gestion.produit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
